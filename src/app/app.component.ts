6e2c741c79f0a8b1c49bdac6f4aedbbcaede2f8b
Web Developer <ts.analista@gmail.com>
2018-11-13 04:44:02 -0200

    Change initialize students list app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Student } from './student/Student.model'
 
 @Component({
   selector: 'jad-root',
@@ -8,9 +9,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'jad';
   tenantCode = 'abc';
-
-  luke = { name: 'Luke', isJedi: true, temple: 'Coruscant' }
-  leia = { name: 'Leia', isJedi: false }
-  han = { name: 'Han Solo', isJedi: false }
-
+  students: Student[] = [
+    { name: 'Luke', isJedi: true, temple: 'Coruscant' },
+    { name: 'Leia', isJedi: false },
+    { name: 'Han Solo', isJedi: false }
+  ]
 }
